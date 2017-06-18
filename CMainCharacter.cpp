381b96faa3ceab2ec2b5feae27b2ecdb51fae90d
LAPTOP-3DUPLS01\Lenovo <leedongsu97@gmail.com>
2017-06-18 20:38:34 +0900

    Main Character

--- CMainCharacter.cpp
+++ CMainCharacter.cpp
@@ -10,18 +10,19 @@ void CMainCharacter::Init() {
 	m_nFrameType = TYPE_WALK;
 	int nTmp = 4;
 	float fTmp = static_cast<float>(nTmp) / 10.f + 1;
-	m_vtCoord = { static_cast<float>(960),210 + static_cast<float>((5 - nTmp) ),fTmp };
+	m_vtCoord = { static_cast<float>(960),210 + static_cast<float>((5 - nTmp)),fTmp };
 	m_nHeight = 57;
 	m_nWidth = 62;
 	SetPosition();
-	m_nDamage = 100;
+	m_nAttack = 100;
+	m_nAttackFrame = 4 * 4;
 };
 
 void CMainCharacter::SetPosition() {
 	m_rcPosition.left = m_vtCoord.GetX() - m_nWidth*(2.2f - (m_vtCoord.GetZ())) + (m_vtCoord.GetZ() - 0.4f) * 10 * 5;
 	m_rcPosition.right = m_vtCoord.GetX() + m_nWidth*(2.2f - (m_vtCoord.GetZ())) + (m_vtCoord.GetZ() - 0.4f) * 10 * 5;
-	m_rcPosition.top = m_vtCoord.GetY() - m_nHeight*(2.2f - (m_vtCoord.GetZ() ));
-	m_rcPosition.bottom = m_vtCoord.GetY() + m_nHeight*(2.2f - (m_vtCoord.GetZ() ));
+	m_rcPosition.top = m_vtCoord.GetY() - m_nHeight*(2.2f - (m_vtCoord.GetZ()));
+	m_rcPosition.bottom = m_vtCoord.GetY() + m_nHeight*(2.2f - (m_vtCoord.GetZ()));
 }
 
 void CMainCharacter::DrawObject(HDC hdc, bool bIntro) {
@@ -35,22 +36,25 @@ void CMainCharacter::Move() {
 	else
 		m_nFrame++;
 }
-
 int CMainCharacter::Attack() {
+
 	if (m_nFrame == m_nAttackFrame) {
 		m_pSoundManager->PlayEffect(EFFECT_ARCHER_ATTACK_00);
-		CVector3D  vtTmp = m_vtCoord;
-		m_pArrow->CreateArrow(new CPlayerArrow(vtTmp, m_nAttack, m_pTarget->ReturnCoord()));
+		CVector3D  vtTmp = m_vtMouse;
+		m_pArrow->CreateArrow(new CPlayerArrow(vtTmp, m_nAttack, m_vtDirect, true));
 	}
 
-		if ((m_nFrame / 4) >= m_sprImg.MaxFrame(m_nFrameType) - 1) {
-			m_nFrame = 0;
-		}
-		else
-		{
-			m_nFrame++;
-		}
-		return 0;
+
+	if (m_nFrame == m_nAttackFrame - 1 && m_bClick)
+		;
+	else if ((m_nFrame / 4) >= m_sprImg.MaxFrame(m_nFrameType) - 1) {
+		SetFrameType(TYPE_WALK);
+	}
+	else
+	{
+		m_nFrame++;
+	}
+	return 0;
 }
 
 void CMainCharacter::SetFrameType(const int &nType) {
@@ -65,11 +69,12 @@ int CMainCharacter::Update() {
 		Move();
 	else if (m_nFrameType == TYPE_ATTACK)
 		tmp = Attack();
-
 	return tmp;
-	
 }
 int CMainCharacter::GetKey(const WPARAM&wParam) {
+	if (m_nFrameType == TYPE_ATTACK)
+		return UNIT_NONE;
+
 	switch (wParam) {
 	case VK_UP:
 	case 'w':
@@ -78,8 +83,8 @@ int CMainCharacter::GetKey(const WPARAM&wParam) {
 			m_vtCoord.SetZ(m_vtCoord.GetZ());
 		}
 		else {
-			m_nMoving={static_cast<float>(0),static_cast<float>(-20),static_cast<float>(1)/10.f };
-		
+			m_nMoving = { static_cast<float>(0),static_cast<float>(-20),static_cast<float>(1) / 10.f };
+
 			m_vtCoord.Move(m_nMoving, true);
 			SetPosition();
 		}
@@ -92,7 +97,7 @@ int CMainCharacter::GetKey(const WPARAM&wParam) {
 		}
 		else {
 			m_nMoving = { static_cast<float>(0),static_cast<float>(20),static_cast<float>(-1) / 10.f };
-			
+
 			m_vtCoord.Move(m_nMoving, true);
 			SetPosition();
 		}
@@ -102,21 +107,36 @@ int CMainCharacter::GetKey(const WPARAM&wParam) {
 	return m_vtCoord.GetZ();
 }
 void CMainCharacter::MouseMove(const LPARAM&lParam) {
-
-
+	m_vtMouse.SetX(LOWORD(lParam));
+	m_vtMouse.SetY(HIWORD(lParam));
+	Ready();
 }
 void CMainCharacter::LButtonDown(const LPARAM&lParam) {
-	m_nFrameType = TYPE_ATTACK;
+	SetFrameType(TYPE_ATTACK);
+	m_bClick = true;
 	m_vtMouse = {
 		static_cast<float>(LOWORD(lParam)),
 		static_cast<float>(HIWORD(lParam)),
 		m_vtCoord.GetZ()
 	};
-
-
+	Ready();
 }
 int CMainCharacter::LButtonUp(const LPARAM&lParam) {
-	m_nFrameType = TYPE_WALK;
-	m_nFrame = 0;
-	return m_nDamage;
+	Shot();
+	m_bClick = false;
+	//SetFrameType(TYPE_ATTACK);
+	//m_nFrameType = TYPE_WALK;
+	//m_nFrame = 0;
+	return m_nAttack;
+}
+void CMainCharacter::Ready() {
+	m_fAngle = atan2f(static_cast<float>(m_vtMouse.GetY()) - m_vtCoord.GetY(), static_cast<float>(m_vtMouse.GetX()) - m_vtCoord.GetX()) * 180 / 3.14;
+	m_vtDirect.SetX(m_nPower*cos(m_fAngle));
+	m_vtDirect.SetY(m_nPower*sin(m_fAngle));
+	m_vtDirect.SetZ(0);
+}
+void CMainCharacter::Shot() {
+	m_vtDirect.SetX(m_nPower*cos(m_fAngle));
+	m_vtDirect.SetY(m_nPower*sin(m_fAngle));
+	m_vtDirect.Move(0, m_fGravity);
 }
--- CMainCharacter.h
+++ CMainCharacter.h
@@ -1,22 +1,24 @@
 #pragma once
 #include"CObject.h"
 #include"CPlayerArrowManager.h"
-#include"CArrow.h"
 
 #define MAXZ 1.4
 #define MINZ 1.0
 class CMainCharacter :public CObject {
-	bool m_bClick;
+	bool m_bClick{ false };
 	CVector3D m_nMoving;
+	float m_fGravity{ 0 };
+	float m_fAngle;				// 화살 쏘는 각도
+	int m_nPower;
 	int m_nDamage;
 	CVector3D m_vtMouse;
-	CPlayerArrowManager* m_pArrow{nullptr};
-	CObject* m_pTarget{nullptr};
+	CPlayerArrowManager* m_pArrow{ nullptr };
+	CObject* m_pTarget{ nullptr };
+	CVector3D m_vtDirect{ 0,0,0 };
 public:
-	CMainCharacter(CSoundManager* sound) : CObject(sound) {
-		
+	CMainCharacter(CSoundManager* sound, CPlayerArrowManager* arrow) : CObject(sound) {
+		m_pArrow = arrow;
 	}
-
 	void Init()override;
 	void DrawObject(HDC hdc, bool bIntro = false)override;
 	void SetPosition()override;
@@ -29,4 +31,6 @@ public:
 	int Update()override final;
 	int Attack()override final;
 	int Dead()override final { return 0; };
+	void Ready();
+	void Shot();
 };
